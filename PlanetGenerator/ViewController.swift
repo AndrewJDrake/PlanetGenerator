c9a619b17402d0f30d7b43e7ddf2534d27d09e78
Michael Oliver <michael.jo.oliver@gmail.com>
2021-04-21 13:37:57 -0500

    Initial Commit

---
+++ PlanetGenerator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlanetGenerator
+//
+//  Created by Michael Oliver on 4/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
